9d05ba319989b1596bcbd59ed05abcc22ef4734e
Thomas Moon <thomasmoon0322@gmail.com>
2024-06-20 12:01:31 -0700

    Add UI to game

--- script.js
+++ script.js
@@ -8,48 +8,43 @@ let humanScore = 0
 let botScore = 0
 
 function playRound(player_choice, bot_choice) {
+    message = document.querySelector(".message")
+    score = document.querySelector(".score")
     player_choice = player_choice.toLowerCase()
     if (player_choice === bot_choice) {
-        console.log("It's a tie!")
+        message.textContent = "It's a tie!"
     }
     else if (player_choice == "rock") {
         if (bot_choice == "scissors") {
-            console.log("You win! Rock beats scissors!")
+            message.textContent = "You win! Rock beats scissors!"
             humanScore++
         }
         else {
-            console.log("You lose! Paper beats rock!")
+            message.textContent = "You lose! Paper beats rock!"
             botScore++
         }
     }
     else if (player_choice == "paper") {
         if (bot_choice == "rock") {
-            console.log("You win! Paper beats rock!")
+            message.textContent = "You win! Paper beats rock!"
             humanScore++
         }
         else {
-            console.log("You lose! Scissors beats paper!")
+            message.textContent = "You lose! Scissors beats paper!"
             botScore++
         }
     }
     else if (player_choice == "scissors") {
         if (bot_choice == "paper") {
-            console.log("You win! Scissors beats paper!")
+            message.textContent = "You win! Scissors beats paper!"
             humanScore++
         }
         else {
-            console.log("You lose! Rock beats scissors!")
+            message.textContent = "You lose! Rock beats scissors!"
             botScore++
         }
     }
-    console.log(`Your Score: ${humanScore}`)
-    console.log(`Computer Score: ${botScore}\n`)
-
-}
-
-function getPlayerChoice() {
-    let player_choice = prompt('Type in "rock", "paper", or "scissors" to make your choice: ')
-    return player_choice
+    score.textContent = `Your Score: ${humanScore} \nComputer Score: ${botScore}`
 }
 
 function getComputerChoice() {
@@ -62,19 +57,14 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 }
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        playRound(getPlayerChoice(), getComputerChoice())
-    }
-    if (humanScore > botScore) {
-        console.log("You Win!")
-    }
-    else {
-        console.log("You Lose!")
-    }
-}
+buttons = document.querySelectorAll("button")
 
-playGame()
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      playRound(button.textContent, getComputerChoice());
+    });
+  });
+
 
 
 
